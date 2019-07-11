cee38e3065166ba91f2edf35a3b1a5b1cfea6cc1
Aleksbgbg <kozichka01@abv.bg>
2019-07-11 11:47:10 +0100

    [wwwroot] Add MainViewModelTests for WpfUnitTest template. Remove template compilation directives in project file.

---
+++ api.iamaleks.dev/wwwroot/Templates/WpfUnitTest/{projectName}.Tests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿namespace {projectName}.Tests.ViewModels
+{
+    using {projectName}.ViewModels;
+
+    public class MainViewModelTests
+    {
+        private readonly MainViewModel _mainViewModel;
+
+        public MainViewModelTests()
+        {
+            _mainViewModel = new MainViewModel();
+        }
+    }
+}
