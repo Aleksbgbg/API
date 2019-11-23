ebb629c7a8802b203edb663d328729faad1ffe28
Aleksbgbg <kozichka01@abv.bg>
2019-11-23 19:16:29 +0000

    [TemplateFileProvider] Re-implement PhysicalFileProvider for templates specifically.

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,7 @@ namespace Api
                     .AddNewtonsoftJson();
 
             services.AddTransient<IProjectQueryProvider, ProjectQueryProvider>();
-            services.AddTransient<IWebRootFileProvider>(_ => new WebRootFileProvider(new PhysicalFileProviderAdapter(_webHostEnvironment.WebRootPath)));
+            services.AddTransient<IWebRootFileProvider>(_ => new WebRootFileProvider(new TemplateFileProvider(_webHostEnvironment.WebRootPath)));
             services.AddTransient<IProjectTemplateProducer, ProjectTemplateProducer>();
             services.AddTransient<IGitFileProvider, GitFileProvider>();
 
