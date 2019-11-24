34aea1809264b1e454a2e0c5d5a7b1e6e418bc80
Aleksbgbg <kozichka01@abv.bg>
2019-11-24 12:08:27 +0000

    [StaticController] Add controller to serve favicon.

---
+++ API/Controllers/StaticController.cs
@@ -0,0 +1,16 @@
+﻿namespace Api.Controllers
+{
+    using System.IO;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    public class StaticController : ControllerBase
+    {
+        [HttpGet("favicon.png")]
+        public PhysicalFileResult GetFavicon()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "favicon.png"), "image/png");
+        }
+    }
+}
